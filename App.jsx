a610557ae6015a5921ed66c039cc29652bf79920
jamelbazzouz-art <jamel.bazzouz@staff.st-chris.net>
2025-09-18 14:12:33 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>French Learning Hub</h1>
+      <p>Your lessons, flashcards, and quizzes will go here.</p>
+    </div>
+  );
+}
